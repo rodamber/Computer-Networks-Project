6f1f81aa7fac8baf38266d76fdeed06eafd00abf
Mariana Ribeiro <msscribeiro@gmail.com>
2015-09-28 23:49:03 +0100

    Added a header file for ECP.

--- src/ECP/ECP.c
+++ src/ECP/ECP.c
@@ -1,20 +1,4 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <string.h>
-
-#define GN 9
-#define ECP_PORT 58000
-#define BUFFER_SIZE 128
-#define TOPIC_SIZE 25
-#define NR_TOPICS 5
-#define AWT_STRING 128
-#define TOPICID_INDEX 4
-#define LINE_READ_SIZE 128
+#include "ecp_utils.h"
 
 int main(int argc, char *argv[]){
 	int option = 0;
---
+++ src/ECP/ecp_utils.h
@@ -0,0 +1,22 @@
+#ifndef __ECP_UTILS_H__
+#define __ECP_UTILS_H__
+
+#define GN 9
+#define ECP_PORT 58000
+#define BUFFER_SIZE 128
+#define TOPIC_SIZE 25
+#define NR_TOPICS 5
+#define AWT_STRING 128
+#define TOPICID_INDEX 4
+#define LINE_READ_SIZE 128
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+
+#endif // __ECP_UTILS_H__
